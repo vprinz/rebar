3f3f77737fcaa4cd0ec797bfbd51b0e0b77f8581
vprinz <vprinz.me@yandex.ru>
2025-06-03 21:18:38 +0100

    - fix problem with background (conflicts with daisyUI)

--- webview/tailwind.config.js
+++ webview/tailwind.config.js
@@ -1,23 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
+import daisyui from 'daisyui';
+
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', '../src/plugins/**/*.{vue,js,ts,jsx,tsx}'],
-    theme: {
-        extend: {
-            animation: {
-                fadein: 'fadeIn .5s ease-in-out',
-                fadeout: 'fadeOut .5s ease-in-out',
-            },
-            keyframes: {
-                fadeIn: {
-                    from: { opacity: 0 },
-                    to: { opacity: 1 },
-                },
-                fadeOut: {
-                    from: { opacity: 1 },
-                    to: { opacity: 0 },
-                },
-            },
-        },
+    plugins: [daisyui],
+    daisyui: {
+        themes: ['light', 'dark', 'cupcake'],
     },
-    plugins: [require('daisyui')],
 };
