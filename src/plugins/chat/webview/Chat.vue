a773c86b8925f0e8cc64464668ae51995c15c63b
pavlikovv <valerypavlikov@yandex.ru>
2025-06-13 14:12:03 +0100

    - add logic to Chat.vue

--- src/plugins/chat/webview/Chat.vue
+++ src/plugins/chat/webview/Chat.vue
@@ -1,42 +1,68 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
-import { Message } from '@Shared/types/message';
-
+import { ref, computed, nextTick, onMounted } from 'vue';
+import { useEvents } from '@Composables/useEvents.js';
+import { useMessenger } from '@Composables/useMessenger.js';
 import ChatMessage from './components/ChatMessage.vue';
+import { ChatConfig } from '../shared/config.js';
+import { ChatEvents } from '../shared/chatEvents.js';
 
+const events = useEvents();
+const { messages, emit } = useMessenger();
+
+const input = ref('');
+const inputBox = ref<HTMLInputElement | null>(null);
 const chatBox = ref<HTMLInputElement | null>(null);
-const demoMessages: Message[] = [
-    {
-        type: 'player',
-        author: 'Valera_Pavlikov',
-        content: 'Привет! Как дела?',
-        timestamp: Date.now(),
-    },
-    {
-        type: 'system',
-        author: '',
-        content: 'Добро пожаловать на сервер!',
-        timestamp: Date.now(),
-    },
-    {
-        type: 'alert',
-        author: '',
-        content: 'Внимание! Сервер будет перезапущен через 5 минут.',
-        timestamp: Date.now(),
-    },
-    {
-        type: 'warning',
-        author: '',
-        content: 'У вас мало HP!',
-        timestamp: Date.now(),
-    },
-    {
-        type: 'info',
-        author: '',
-        content: 'Сегодня x2 опыт для новичков!',
-        timestamp: Date.now(),
-    },
-];
+const timestamp = ref(true);
+const focused = ref(true);
+
+const chatMessages = computed(() => {
+    if (messages.value.length < ChatConfig.messagesPerPage) {
+        return [...messages.value].reverse();
+    }
+
+    if (!focused.value) {
+        return messages.value.slice(0, ChatConfig.messagesPerPage).reverse();
+    }
+
+    return [...messages.value].reverse();
+});
+
+function focus() {
+    focused.value = true;
+    inputBox.value.focus();
+
+    nextTick(() => {
+        const child = chatBox.value.children[chatBox.value.children.length - 1];
+        if (!child) {
+            return;
+        }
+
+        child.scrollIntoView({ behavior: 'instant' });
+    });
+}
+
+function unfocus() {
+    focused.value = false;
+    inputBox.value.blur();
+}
+
+function send() {
+    focused.value = false;
+    inputBox.value.blur();
+
+    if (input.value.length <= 0) {
+        return;
+    }
+
+    emit(input.value);
+    input.value = '';
+}
+
+onMounted(() => {
+    events.on(ChatEvents.toWebview.focus, focus);
+    events.on(ChatEvents.toWebview.unfocus, unfocus);
+    events.on(ChatEvents.toWebview.send, send);
+});
 </script>
 
 <template>
@@ -45,11 +71,21 @@ const demoMessages: Message[] = [
             class="flip-left flex max-h-[448px] min-h-[448px] min-w-[448px] max-w-[448px] flex-col gap-4 overflow-y-auto pl-3"
             ref="chatBox"
         >
-            <ChatMessage v-for="(msg, idx) in demoMessages" :key="idx" :message="msg" :timestamp="true" />
+            <ChatMessage v-for="(msg, idx) in chatMessages" :key="idx" :message="msg" :timestamp="timestamp" />
         </div>
         <div class="flex w-full flex-col gap-4">
-            <label class="input input-bordered flex items-center gap-2">
-                <input type="text" class="grow" placeholder="Write text or /command" />
+            <label
+                class="input input-bordered flex items-center gap-2"
+                :class="focused ? ['opacity-85'] : ['opacity-0']"
+            >
+                <input
+                    :max="ChatConfig.inputLength"
+                    v-model="input"
+                    ref="inputBox"
+                    type="text"
+                    class="grow"
+                    placeholder="Write text"
+                />
                 <kbd class="kbd kbd-sm">Enter</kbd>
             </label>
         </div>
