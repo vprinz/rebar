b6e2f78dbe887e816027408d20a698e55a076ee8
vprinz <vprinz.me@yandex.ru>
2025-06-19 16:26:37 +0100

    - register CurrencyService

--- src/plugins/currency/server/index.ts
+++ src/plugins/currency/server/index.ts
@@ -3,82 +3,35 @@ import { useRebar } from '@Server/index.js';
 
 const Rebar = useRebar();
 
-const API_NAME = 'currency-api';
 type CurrencyType = 'cash' | 'bank';
 
-function useCurrency(player: alt.Player) {
-    const document = Rebar.document.character.useCharacter(player);
+Rebar.services.useServiceRegister().register('currencyService', {
+    async add(player: alt.Player, type: CurrencyType, quantity: number) {
+        if (quantity < 0) return false;
 
-    if (!document.isValid()) return undefined;
+        const currentAmount = await this.get(player, type);
+        const newAmount = Math.min(currentAmount + quantity, Number.MAX_SAFE_INTEGER);
 
-    async function updateCurrency(type: CurrencyType, newAmount: number): Promise<boolean> {
-        if (!['bank', 'cash'].includes(type)) return false;
-        await document.set(type, newAmount);
+        await Rebar.document.character.useCharacter(player).set(type, newAmount);
         return true;
-    }
+    },
+    async sub(player: alt.Player, type: CurrencyType, quantity: number) {
+        if (quantity <= 0) return false;
 
-    /**
-     * Add a specific amount of currency to the player.
-     *
-     * @param {CurrencyType} type - The type of currency to add.
-     * @param {number} amount
-     * @returns {boolean} - Returns true if the operation was successful, false otherwise.
-     */
-    async function add(type: CurrencyType, amount: number): Promise<boolean> {
-        if (amount <= 0) return false;
+        const currentAmount = await this.get(player, type);
+        if (currentAmount < quantity) return false;
 
-        const currentAmount = get(type);
-        const amountChangedTo = Math.min(currentAmount + amount, Number.MAX_SAFE_INTEGER);
-
-        return updateCurrency(type, amountChangedTo);
-    }
-
-    /**
-     * Subtracts a given currency type from an amount.
-     *
-     * @param {CurrencyType} type - The type of currency to add.
-     * @param {number} amount
-     * @returns {boolean} - Returns true if the operation was successful, false otherwise.
-     */
-    async function subtract(type: CurrencyType, amount: number): Promise<boolean> {
-        if (amount <= 0) return false;
-
-        const currentAmount = get(type);
-        if (currentAmount < amount) return false;
-
-        const newAmount = currentAmount - amount;
-
-        return updateCurrency(type, newAmount);
-    }
-
-    /**
-     * Get the current amount of currency the player has.
-     *
-     * * @param {CurrencyType} type - The type of currency to get.
-     * @returns {number}
-     */
-    function get(type: CurrencyType): number {
-        const character = document.get();
-        return character[type] || 0;
-    }
-
-    return {
-        add,
-        subtract,
-        get,
-    };
-}
-
-function useApi() {
-    return {
-        useCurrency,
-    };
-}
-
-declare global {
-    export interface ServerPlugin {
-        [API_NAME]: ReturnType<typeof useApi>;
-    }
-}
-
-Rebar.useApi().register(API_NAME, useApi());
+        const newAmount = currentAmount - quantity;
+        await Rebar.document.character.useCharacter(player).set(type, newAmount);
+        return true;
+    },
+    async has(player, type, quantity) {
+        if (quantity <= 0) return false;
+        const currentAmount = await this.get(player, type);
+        return currentAmount >= quantity;
+    },
+    async get(player: alt.Player, type: CurrencyType) {
+        const document = Rebar.document.character.useCharacter(player);
+        return document.get()[type] || 0;
+    },
+});
